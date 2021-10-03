547296fc349d4f7371d32ef1b1f58e1ace7836fd
Éllen Neves <ellenosneves@gmail.com>
2021-10-03 14:28:12 -0300

    Update and rename trabalho02.py to registraMultas.py

--- registraMultas.py
+++ registraMultas.py
@@ -1,5 +1,5 @@
 '''
-Éllen Oliveira Silva Neves (20202BSI0071)
+Éllen Neves (ellenosneves@gmail.com)
 Trabalho solicitado pelo professor da disciplina de Programação II, Hilário Seibel Junior
 '''
 
